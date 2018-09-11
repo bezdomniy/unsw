a623d54a0d1928db1673c2c2c19c86e4144e7497
Ilia Chibaev <ilia.shibaev@gmail.com>
2018-09-12 09:52:35 +1100

    trying now with vector instead
    Former-commit-id: fd80670db9118320acb2ad3405e947f9f0801eb0

--- COMP9319/ass2/bwtencode.cpp
+++ COMP9319/ass2/bwtencode.cpp
@@ -1,67 +1,51 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <algorithm>
 #include <stdlib.h>
 using namespace std;
-
-
 
 int getFileSize(string fileName) {
     ifstream input(fileName, ifstream::binary | ifstream::ate);
     return input.tellg();
 }
 
-int circlularMemcmp(const unsigned char* p1, const unsigned char* p2, size_t lengthToEnd, size_t lengthFromStart) {
-    if (lengthToEnd == 0 && lengthFromStart == 0)
-        return 0;
 
-    int i = 0;
-    while (i < lengthToEnd) {
-        if (*p1 != *p2)
-            return *p1 - *p2;
-        p1++;
-        p2++;
-        i++;
-    }
-    //if ()
-    
-}
-
-#define BUFFERLENGTH 8
+#define BUFFERLENGTH 40
 char buffer[BUFFERLENGTH];
 unsigned int rotationIndices[BUFFERLENGTH];
 
 int circularCompare(const unsigned int *i1, const unsigned int *i2) {
-    unsigned int endL1 = (unsigned int) (BUFFERLENGTH - *i1 + 1);
-    unsigned int endL2 = (unsigned int) (BUFFERLENGTH - *i2 + 1);
-        
-    int indexToEnd = memcmp(buffer + *i1,buffer + *i2, endL1 < endL2 ? endL1 : endL2);
+    unsigned int endL1 = (unsigned int) (BUFFERLENGTH - *i1 );
+    unsigned int endL2 = (unsigned int) (BUFFERLENGTH - *i2 );
 
-    cout << *i1<< buffer + *i1 << " | " << *i2 << buffer + *i2 << " : res: " << indexToEnd  << "\n";
-
-    if (indexToEnd == 0) {
-        if (endL1 < endL2) {
-            int toEndOfFirst = memcmp(buffer, buffer + endL1, BUFFERLENGTH - endL1);
-            if (toEndOfFirst == 0) {
-                return memcmp(buffer + endL2, buffer, BUFFERLENGTH - endL2);
-            }
-            return toEndOfFirst;
+    if (endL1 < endL2) {
+        int bothWrappedAround = memcmp(buffer + endL1, buffer + endL2, BUFFERLENGTH - endL2);
+        //cout << buffer + endL1 << " | " << buffer + endL2 << " : len: " << BUFFERLENGTH - endL2 << " res: " << bothWrappedAround  << "\n";
+        if (bothWrappedAround == 0) {
+            int oneWrapAround = memcmp(buffer + endL1 + BUFFERLENGTH - endL2, buffer, BUFFERLENGTH - endL1);
+            //cout << "deeper1: " << buffer + endL1 + BUFFERLENGTH - endL2 << " | " << buffer << " : len: " << BUFFERLENGTH - endL1 << " res: " << oneWrapAround  << "\n";
+            if (oneWrapAround == 0)
+                return memcmp(buffer + *i1,buffer + *i2, endL1);
+            return oneWrapAround;
         }
-        else {
-            int toEndOfFirst = memcmp(buffer + endL2, buffer, BUFFERLENGTH - endL2);
-            if (toEndOfFirst == 0) {
-                return memcmp(buffer + endL1, buffer, BUFFERLENGTH - endL1);
-            }
-            return toEndOfFirst;
-        }
+        return bothWrappedAround;
     }
-
-    return indexToEnd;
+    else {
+        int bothWrappedAround = memcmp(buffer + endL1, buffer + endL2, BUFFERLENGTH - endL1);
+        //cout << buffer + endL1 << " | " << buffer + endL2 << " : len: " << BUFFERLENGTH - endL1 << " res: " << bothWrappedAround  << "\n";
+        if (bothWrappedAround == 0) {
+            int oneWrapAround = memcmp(buffer, buffer + endL2 + BUFFERLENGTH - endL1, BUFFERLENGTH - endL2);
+            //cout << "deeper2: " << buffer + endL2 + BUFFERLENGTH - endL1 << " | " << buffer << " : len: " << BUFFERLENGTH - endL2 << " res: " << oneWrapAround  << "\n";
+            if (oneWrapAround == 0)
+                return memcmp(buffer + *i1,buffer + *i2, endL2);
+            return oneWrapAround;
+        }
+        return bothWrappedAround;
+    }
+        
 };
 
-void printBWT(char str[], int index[1]) {
-
-}
 
 main(int argc, char const *argv[])
 {
@@ -71,41 +55,68 @@ main(int argc, char const *argv[])
     ifstream input(fileName);
     input.unsetf(ios_base::skipws);
 
+    ofstream output;
+    output.open("./example1.bwt");
+
     //char inputString[fileLength];
     //unsigned int rotationIndices[fileLength];
 
-    unsigned int pos = 0;
-    while (input) {
-        input >> buffer[pos];
-        rotationIndices[pos] = pos;
-        pos++;
-    }
+    int count = 1;
+    unsigned int* indexPointer = rotationIndices;
+    while (input.peek() != EOF) {
+        unsigned int pos = 0;
+        while (input) {
+            input >> buffer[pos];
+            rotationIndices[pos] = pos;
+            pos++;
 
-    qsort(rotationIndices, BUFFERLENGTH, sizeof(unsigned int),
-            (int(*)(const void *, const void *))circularCompare);
+            if (pos == BUFFERLENGTH)
+                break;
+        }
 
-    for (const auto i: rotationIndices) {
-        cout << buffer[BUFFERLENGTH-i-1];
+        //unsigned int* indexPointer = rotationIndices;
+
+        if (pos == BUFFERLENGTH) {
+            qsort(rotationIndices, BUFFERLENGTH, sizeof(unsigned int),
+                (int(*)(const void *, const void *))circularCompare);
+        }
+        else {
+            unsigned int trimmedRotationIndicies[pos];
+            copy_n(rotationIndices, pos, trimmedRotationIndicies);
+            qsort(trimmedRotationIndicies, pos, sizeof(unsigned int),
+                (int(*)(const void *, const void *))circularCompare);
+            indexPointer = trimmedRotationIndicies;
+        }
+
+
+        //for (const auto i: rotationIndices) {
+        for (int i = 0; i < BUFFERLENGTH; i++) {
+            if (i > pos)
+                break;
+            cout << buffer[BUFFERLENGTH-rotationIndices[i]-1];
+        }
+
+        cout << count * 10000 << "\n";
+        count++;
     }
+    output.close();
+
+    // for (const auto c:buffer) {
+    //     cout << c;
+    // }
+    // cout << "|\n";
+
     cout << "\n";
-    for (int i = 0; i <= BUFFERLENGTH; i++) {
-        cout << buffer[i];
+    //for (const auto i:indexPointer) {
+    for (int i = 0; i < BUFFERLENGTH; i++) {
+        cout << *(indexPointer+i) << " ";
     }
-    cout << "\n";
 
-    for (const auto i:rotationIndices) {
-        cout << i << " ";
-    }
-    cout << BUFFERLENGTH;
+    vector<int> v = {1,2,3};
+    v.resize(2);
+    for (const auto i: v) cout << i;
+
     return 0;
+
 }
 
-
-
-//     #include <stdio.h>
-// #include <iostream>
-// #include <map>
-// #include <algorithm>
-// #include <vector>
-// #include <queue>
-// #include <stack>;
