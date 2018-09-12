0a5df6178fde9ea7ad37b2bc39ec786bcb83e0aa
Ilia Chibaev <ilia.shibaev@gmail.com>
2018-09-12 12:08:01 +1100

    suffix implementation working
    Former-commit-id: 7bfe26ae54c9296557191fd208712a03c2c43c47

--- COMP9319/ass2/bwtencode.cpp
+++ COMP9319/ass2/bwtencode.cpp
@@ -11,19 +11,32 @@ int getFileSize(string fileName) {
 }
 
 
-#define BUFFERLENGTH 40
+#define BUFFERLENGTH 50000000
+int currentEnd = BUFFERLENGTH;
 char buffer[BUFFERLENGTH];
-unsigned int rotationIndices[BUFFERLENGTH];
+vector<unsigned int> rotationIndices(BUFFERLENGTH);
+
+int suffixCompare(const unsigned int *i1, const unsigned int *i2) {
+    unsigned int endL1 = (unsigned int) (currentEnd - *i1 );
+    unsigned int endL2 = (unsigned int) (currentEnd - *i2 );
+
+    int res = memcmp(buffer + *i1,buffer + *i2, endL1 < endL2 ? endL1 : endL2);
+    if ( res == 0 )
+        return endL1 - endL2;
+    else
+        return res;
+
+};
 
 int circularCompare(const unsigned int *i1, const unsigned int *i2) {
-    unsigned int endL1 = (unsigned int) (BUFFERLENGTH - *i1 );
-    unsigned int endL2 = (unsigned int) (BUFFERLENGTH - *i2 );
+    unsigned int endL1 = (unsigned int) (currentEnd - *i1 );
+    unsigned int endL2 = (unsigned int) (currentEnd - *i2 );
 
     if (endL1 < endL2) {
-        int bothWrappedAround = memcmp(buffer + endL1, buffer + endL2, BUFFERLENGTH - endL2);
+        int bothWrappedAround = memcmp(buffer + endL1, buffer + endL2, currentEnd - endL2);
         //cout << buffer + endL1 << " | " << buffer + endL2 << " : len: " << BUFFERLENGTH - endL2 << " res: " << bothWrappedAround  << "\n";
         if (bothWrappedAround == 0) {
-            int oneWrapAround = memcmp(buffer + endL1 + BUFFERLENGTH - endL2, buffer, BUFFERLENGTH - endL1);
+            int oneWrapAround = memcmp(buffer + endL1 + currentEnd - endL2, buffer, currentEnd - endL1);
             //cout << "deeper1: " << buffer + endL1 + BUFFERLENGTH - endL2 << " | " << buffer << " : len: " << BUFFERLENGTH - endL1 << " res: " << oneWrapAround  << "\n";
             if (oneWrapAround == 0)
                 return memcmp(buffer + *i1,buffer + *i2, endL1);
@@ -32,89 +45,78 @@ int circularCompare(const unsigned int *i1, const unsigned int *i2) {
         return bothWrappedAround;
     }
     else {
-        int bothWrappedAround = memcmp(buffer + endL1, buffer + endL2, BUFFERLENGTH - endL1);
+        int bothWrappedAround = memcmp(buffer + endL1, buffer + endL2, currentEnd - endL1);
         //cout << buffer + endL1 << " | " << buffer + endL2 << " : len: " << BUFFERLENGTH - endL1 << " res: " << bothWrappedAround  << "\n";
         if (bothWrappedAround == 0) {
-            int oneWrapAround = memcmp(buffer, buffer + endL2 + BUFFERLENGTH - endL1, BUFFERLENGTH - endL2);
+            int oneWrapAround = memcmp(buffer, buffer + endL2 + currentEnd - endL1, currentEnd - endL2);
             //cout << "deeper2: " << buffer + endL2 + BUFFERLENGTH - endL1 << " | " << buffer << " : len: " << BUFFERLENGTH - endL2 << " res: " << oneWrapAround  << "\n";
             if (oneWrapAround == 0)
                 return memcmp(buffer + *i1,buffer + *i2, endL2);
             return oneWrapAround;
         }
         return bothWrappedAround;
-    }
-        
+    }   
 };
 
 
 main(int argc, char const *argv[])
 {
-    string fileName = "./example1.txt";
+    string fileName = "./warandpeace3.txt";
     //long fileLength = getFileSize(fileName);
 
     ifstream input(fileName);
     input.unsetf(ios_base::skipws);
 
     ofstream output;
-    output.open("./example1.bwt");
+    output.open("./warandpeace3.bwt");
 
     //char inputString[fileLength];
     //unsigned int rotationIndices[fileLength];
 
     int count = 1;
-    unsigned int* indexPointer = rotationIndices;
+
     while (input.peek() != EOF) {
         unsigned int pos = 0;
-        while (input) {
+        while (input.peek() != EOF && pos < BUFFERLENGTH) {
             input >> buffer[pos];
+            //rotationIndices.at(pos) = pos;
             rotationIndices[pos] = pos;
             pos++;
-
-            if (pos == BUFFERLENGTH)
-                break;
         }
-
-        //unsigned int* indexPointer = rotationIndices;
 
         if (pos == BUFFERLENGTH) {
-            qsort(rotationIndices, BUFFERLENGTH, sizeof(unsigned int),
-                (int(*)(const void *, const void *))circularCompare);
+            qsort(&rotationIndices[0], BUFFERLENGTH, sizeof(unsigned int),
+                (int(*)(const void *, const void *))suffixCompare);
         }
         else {
-            unsigned int trimmedRotationIndicies[pos];
-            copy_n(rotationIndices, pos, trimmedRotationIndicies);
-            qsort(trimmedRotationIndicies, pos, sizeof(unsigned int),
-                (int(*)(const void *, const void *))circularCompare);
-            indexPointer = trimmedRotationIndicies;
+            currentEnd = pos;
+            rotationIndices.resize(pos);
+            qsort(&rotationIndices[0], pos, sizeof(unsigned int),
+                (int(*)(const void *, const void *))suffixCompare);
         }
 
 
-        //for (const auto i: rotationIndices) {
-        for (int i = 0; i < BUFFERLENGTH; i++) {
-            if (i > pos)
-                break;
-            cout << buffer[BUFFERLENGTH-rotationIndices[i]-1];
+        // subtract 1 from suffix index to get bwt
+        for (int i = 0; i < currentEnd; i++) {
+            if (rotationIndices[i] > 0)
+                rotationIndices[i]--;
+            else
+                rotationIndices[i] = pos-1;
         }
 
-        cout << count * 10000 << "\n";
+
+        for (int i = 0; i < currentEnd; i++) {
+            //cout << buffer[currentEnd-rotationIndices[i]-1];
+            output << buffer[rotationIndices[i]];
+        }
+
+        cout << count * BUFFERLENGTH << "\n";
         count++;
     }
     output.close();
 
-    // for (const auto c:buffer) {
-    //     cout << c;
-    // }
-    // cout << "|\n";
 
     cout << "\n";
-    //for (const auto i:indexPointer) {
-    for (int i = 0; i < BUFFERLENGTH; i++) {
-        cout << *(indexPointer+i) << " ";
-    }
-
-    vector<int> v = {1,2,3};
-    v.resize(2);
-    for (const auto i: v) cout << i;
 
     return 0;
 
