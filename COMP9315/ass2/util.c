6a6ce367ab9f74e2bab19fa5733eadec163dbc2e
Ilia <ilia.shibaev@gmail.com>
2019-07-24 10:43:18 +1000

    starting assignment 2

---
+++ COMP9315/ass2/util.c
@@ -0,0 +1,15 @@
+// util.h ... interface to various useful functions
+// part of Multi-attribute Linear-hashed Files
+// Functions that don't fit into one of the
+//   obvious data types like File, Query, ...
+// Last modified by John Shepherd, July 2019
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void fatal(char *msg)
+{
+	fprintf(stderr,"%s\n",msg);
+	exit(1);
+}
